e3e42cef6cf8adeffa96136d69f2b8ff6ed9d25c
ZZANZZAN <40537652+ZZANZZAN@users.noreply.github.com>
2020-04-15 18:57:26 +0300

    Create Filling_from_file_csv.h

---
+++ LAB_10/Filling_from_file_csv.h
@@ -0,0 +1,7 @@
+#ifndef FILLING_FROM_FILE_CSV_H_INCLUDED
+#define FILLING_FROM_FILE_CSV_H_INCLUDED
+
+char **simple_split(char *str, int length, char sep);
+void ClearStringArray(char **str, int n);
+
+#endif // FILLING_FROM_FILE_CSV_H_INCLUDED
